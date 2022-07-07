045776b92074a282babb901bbd93d04c59da4ec4
raquelbcarballo <80289565+raquelbcarballo@users.noreply.github.com>
2022-07-07 08:31:28 +0200

    Update App.vue

--- web/src/App.vue
+++ web/src/App.vue
@@ -22,4 +22,18 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+:root {
+  --cardRadius: 1em;
+  --cardMargin: 1em;
+  --cardPadding: 1em;
+  --extrasmallTextSize: 50%;
+  --smallTextSize: 75%;
+  --mediumTextSize: 100%;
+  --largeTextSize: 150%;
+  --extralargeTextSize: 200%;
+  --primaryColor: #045775;
+  --secondaryColor: #49b60e;
+  --longButton: 3%;
+  --boxButton: 1em;
+}
 </style>
